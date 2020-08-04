de09a4a9a3c08f23540d79b4c9fa9db94ceec006
Gilderin <hotabi4-96@yandex.ru>
2020-08-04 21:21:21 +0300

    add test

--- src/test/java/tests/MainTests.java
+++ src/test/java/tests/MainTests.java
@@ -2,6 +2,7 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -13,6 +14,19 @@ public class MainTests {
     {
         System.out.println("selenium tests");
         driver=new ChromeDriver();
+        driver.get("https://google.com");
+
+        System.out.println(driver.getTitle());
+        Assert.assertEquals(driver.getTitle(),"Google");
+        driver.close();
+
+    }
+
+    @Test
+    public void openFirefoxpage()
+    {
+        System.out.println("selenium tests");
+        driver=new FirefoxDriver();
         driver.get("https://google.com");
 
         System.out.println(driver.getTitle());
